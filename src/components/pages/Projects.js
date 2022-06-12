bcba8c6922a306131db552553b1ef5f414d48a4d
javogc <javo_gc@hotmail.com>
2022-06-12 15:21:21 -0500

    check in

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,9 +5,11 @@ function Projects() {
     <div>
       <Container>
         <h1>Projects</h1>
+        <p>12/06/2022</p>
         <p>
-          Page in development. You can find links to my Resume, Github and
-          LinkedIn on the navbar.
+          I'm still thinking about the layout I want for my projects page. But
+          in the meantime, I'm building a sample project in this page that
+          showcases some of my frontend skills.
         </p>
       </Container>
     </div>
