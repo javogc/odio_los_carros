a027d34b0dc0673f8c9dd2cb9dac8bac9172b48f
Javier Guajardo <javo_gc@hotmail.com>
2024-06-11 14:20:33 -0600

    Adds ScrollRestoration to Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <NavBar />
+      <ScrollRestoration />
       <Outlet />
     </>
   );
