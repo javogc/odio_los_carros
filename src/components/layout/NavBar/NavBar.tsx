24ad885b9d93dad2c4ff4f489beef2e59bfeb19e
Javier Guajardo <javo_gc@hotmail.com>
2024-10-07 20:17:10 -0600

    Installs tailwind, set-up tailwind.config and updates home page copy

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./NavBar.css";
-import logo from "../../../images/logo1.png";
+// import "./NavBar.css";
+// import logo from "../../../images/logo1.png";
 
 export default function NavBar() {
   return (
     <nav>
-      <img src={logo} style={{ width: "10%%" }}></img>
+      {/* <img src={logo} style={{ width: "10%" }}></img> */}
       <div>
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,62 +1,33 @@
 import React from "react";
-import "./HomePage.css";
+// import "./HomePage.css";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="homeContainer">
-      <div>
-        <div>
-          <h4>
-            Hey there! I'm <b>Javier Guajardo</b>, a full-stack web developer
-            based in Mexico City with a knack for crafting impactful digital
-            experiences. Currently freelancing,{" "}
-            <b>
-              I specialize in building intuitive data dashboards, interactive
-              maps, and a wide range of custom web applications{" "}
-            </b>{" "}
-            that empower users with insights and engage them on a deeper level.
-            Passionate about user-centric design and continuous learning, I
-            thrive on collaborating closely with clients to bring their visions
-            to life. <br></br>
-            <br></br>My{" "}
-            <Link to="/projects" className="homeLink">
-              portfolio{" "}
-            </Link>
-            showcases a diverse range of projects, each reflecting my commitment
-            to creativity, functionality, and seamless user experiences. While
-            I'm dedicated to freelance work, I'm also keen on exploring new
-            opportunities where I can contribute my skills to innovative teams.
-            If you're seeking a reliable partner for your next project or a
-            dynamic developer to join your team, let's connect and explore the
-            possibilities. Let's make something extraordinary together.{" "}
-            <br></br>
-            <br></br>
-            <a className="homeLink" href="mailto:javiergc93@gmail.com">
-              Reach out
-            </a>
-            {""}, and let's turn your ideas into reality!
-          </h4>
-        </div>
-      </div>
-
-      {/* {language === "en" && (
-        <div>
-          <div>
-            <h1>Javier Guajardo</h1>
-            <h2>I'm updating this website</h2>
-            <h5>Coming soon: projects section</h5>
-          </div>
-          <div>
-            <h5>
-              For inquiries or invoices:{" "}
-              <a className="homeLink" href="mailto:javiergc93@gmail.com">
-                Contact Me
-              </a>
-            </h5>
-          </div>
-        </div>
-      )} */}
+      <p>
+        ¡Hola! Soy Javier Guajardo, un desarrollador web full-stack de la Ciudad
+        de México, y estoy detrás de Odio los Carros. Este proyecto busca
+        cuestionar cómo usamos la tecnología en nuestras vidas. Creo firmemente
+        que la tecnología no solo debe servir a los humanos, sino que también
+        está presente en la naturaleza, como el micelio, que toma decisiones
+        complejas para apoyar su ecosistema. Para mí, la tecnología es un
+        esfuerzo multi-especie que debe estar al servicio de toda la vida en
+        nuestro planeta. Me encanta crear aplicaciones web que utilizan datos
+        para ayudar a los usuarios a obtener información valiosa y fomentar
+        prácticas sostenibles. Hago de todo: desde tableros interactivos y mapas
+        en tiempo real hasta soluciones a medida. Para mí, la tecnología es una
+        herramienta para resolver problemas y hacer que nuestras vidas sean un
+        poco mejores, reconociendo que somos parte de un ecosistema más amplio.
+        Uso tecnologías como React, D3.js, Leaflet, Firebase y Supabase para
+        ofrecer aplicaciones innovadoras que realmente importan. Soy un
+        apasionado del diseño centrado en el usuario y siempre busco cómo hacer
+        que las cosas sean más amigables. Me gusta colaborar con personas que
+        también creen en el poder de la tecnología para generar un cambio
+        positivo. Si tienes una idea o un proyecto en mente y quieres explorar
+        cómo podemos hacerlo realidad, no dudes en ponerte en contacto.
+        ¡Hablemos y veamos qué podemos crear juntos!
+      </p>
     </div>
   );
 }
