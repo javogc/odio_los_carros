30f7bc45d8323c403b2ffe80bd571d3d6d9342db
Javier Guajardo <javo_gc@hotmail.com>
2024-10-10 14:48:12 -0600

    Updates ProjectListItem to use tailwind

--- src/components/pages/Projects/ProjectListItem/ProjectListItem.tsx
+++ src/components/pages/Projects/ProjectListItem/ProjectListItem.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./ProjectListItem.css";
 import { ProjectType } from "../../../../utils/projects";
 import { Link } from "react-router-dom";
 
@@ -12,14 +11,19 @@ export default function ProjectListItem({ project }: ProjectListItemProps) {
   const { title, company, type, id, year } = project;
 
   return (
-    <div className="listItem">
+    <div className="text-center text-lato p-[30px] hover:bg-[#dcdad736] cursor-pointer listItem">
       <h2>
-        <Link to={`/projects/${id}`}>{title}</Link>
+        <Link
+          to={`/projects/${id}`}
+          className="text-odioWhite font-robotoMono underline font-bold text-[3rem] cursor-auto hover:text-odioHover hover:cursor-inherit"
+        >
+          {title}
+        </Link>
       </h2>
-      <h5>
+      <h5 className="font-robotoMono text-odioWhite">
         {company}, {year}
       </h5>
-      <h5>{type}</h5>
+      <h5 className="font-robotoMono text-odioWhite">{type}</h5>
     </div>
   );
 }
